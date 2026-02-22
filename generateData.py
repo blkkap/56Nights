fdc160fbd39919d5a73c5a5766d4a94183df3bb3
anonymousEROS <dovala69@gmail.com>
2026-02-21 18:58:43 -0700

    basic data process

--- generateData.py
+++ generateData.py
@@ -1,2 +1,20 @@
+import pandas as pd
+import os
+
+
 def getData(file):
+    dirName = 'data/'
+    for contents in os.listdir(dirName):
+            with open(os.path.join(dirName, file),'r') as f:
+
+                df = pd.read_csv(f)
+    print(df.head())
+    return 
+
+
+if __name__=='__main__':
+    file = 'MRegularSeasonDetailedResults.csv'
+    getData(file)
+
+    
     
