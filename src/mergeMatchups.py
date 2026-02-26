6dc6402d65a3458ca6fee3ffaad83e9933402443
anonymousEROS <dovala69@gmail.com>
2026-02-25 23:45:49 -0700

    merged all

---
+++ src/mergeMatchups.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import os
+
+
+def mergeMatchups(file, file2):
+    file = pd.read_csv(f'../data/preprocess/{file}')
+    file2 = pd.read_csv(f'../data/preprocess/{file2}')
+    df = pd.concat([file, file2])
+    df.to_csv('../data/preprocess/merged_team_matchups.csv', index=False)
+    print(df.head())
+
+
+
+if __name__=='__main__':
+    file1 = 'Team_Matchups.csv'
+    file2 = 'W_Team_Matchups.csv'
+
+    mergeMatchups(file1, file2)
