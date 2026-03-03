d3c2dd356ba207c9e01d09ecf703870f3f8d005a
anonymousEROS <dovala69@gmail.com>
2026-03-03 03:02:15 -0700

    new samp

--- src/subData.py
+++ src/subData.py
@@ -4,6 +4,11 @@ import pandas as pd
 def subCSV(file1,file2):
     sample = pd.read_csv(f'../data/raw/{file1}')
     season = pd.read_csv(f'../data/preprocess/{file2}')
+    sample[['Season','Team1', 'Team2']] = sample['ID'].str.split('_', expand=True)
+    sample['Season'] = sample['Season'].astype(int)
+    sample['Team1'] = sample['Team1'].astype(int)
+    sample['Team2'] = sample['Team2'].astype(int)
+    sample.to_csv('../data/preprocess/sampleSub.csv', index=False)
     #with open(os.path.join(path1,file1), 'r') as f:
     print(sample.head())
     print(season.head())
