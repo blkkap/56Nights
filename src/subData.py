e16b7d5b154440b934a4e7c245733b4214352be9
anonymousEROS <dovala69@gmail.com>
2026-03-03 04:37:32 -0700

    WSeason

--- src/subData.py
+++ src/subData.py
@@ -1,9 +1,10 @@
 import os
 import pandas as pd 
 
-def subCSV(file1,file2):
+def subCSV(file1,file2,file3):
     sample = pd.read_csv(f'../data/raw/{file1}')
-    season = pd.read_csv(f'../data/preprocess/{file2}')
+    Mseason = pd.read_csv(f'../data/preprocess/{file2}')
+    Wseason = pd.read_csv(f'../data/preprocess/{file3}')
     sample[['Season','Team1', 'Team2']] = sample['ID'].str.split('_', expand=True)
     sample['Season'] = sample['Season'].astype(int)
     sample['Team1'] = sample['Team1'].astype(int)
@@ -12,7 +13,8 @@ def subCSV(file1,file2):
     sample.to_csv('../data/preprocess/sampleSub.csv', index=False)
     #with open(os.path.join(path1,file1), 'r') as f:
     print(sample.head())
-    print(season.head())
+    print(Mseason.head())
+    print(Wseason.head())
     return
 
 
