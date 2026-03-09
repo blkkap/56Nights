a400dc625c214b094e24fecd330ba15515e23c7b
blkkap <dovala69@gmail.com>
2026-03-09 15:45:34 -0700

    quick fix

--- models/xgbModel.py
+++ models/xgbModel.py
@@ -30,7 +30,7 @@ for i in range(3, len(allSeasons)):
     y_test = test_df[target]
 
 
-    NESTIMATOR= 1000
+    NESTIMATOR= 1500
     MAXDEPTH = 3 
     LR = 0.01
     OBJ = 'binary:logistic'
@@ -50,7 +50,7 @@ for i in range(3, len(allSeasons)):
             eval_metric = EVALMETRICS,
             random_state = RANDOMSTATE,
             tree_method = TREEMETHOD,
-            early_stopping_rounds=EARLYSTOPPINGROUNDS
+            #early_stopping_rounds=EARLYSTOPPINGROUNDS
         )
     model.fit(X_train, y_train, eval_set=[(X_test,y_test)])
     preds = model.predict_proba(X_test)[:, 1]
