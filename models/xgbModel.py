d8be5465930b73ea6b24f705bd0bf7a7355bbf37
blkkap <dovala69@gmail.com>
2026-03-12 21:44:29 -0700

    tune

--- models/xgbModel.py
+++ models/xgbModel.py
@@ -18,7 +18,7 @@ allSeasons = sorted(df['Season'].unique())
 logloss_res = {}
 for i in range(3, len(allSeasons)):
     testSeason = allSeasons[i]
-    train_seasons = allSeasons[:i]
+    train_seasons = allSeasons[:i-1]
 
     train_df = df[df['Season'].isin(train_seasons)]
     test_df = df[df['Season'] == testSeason]
@@ -30,17 +30,18 @@ for i in range(3, len(allSeasons)):
     y_test = test_df[target]
 
 
-    NESTIMATOR= 700
-    MAXDEPTH = 2
+    NESTIMATOR= 4000
+    MAXDEPTH = 4
     #MINCHILDWEIGHT = 4 
-    LR = 0.01
+    LR = 0.03
     OBJ = 'binary:logistic'
     SUBSAMPLE = 0.8
     COLSAMPLEBYTREE = 0.8 
     EVALMETRICS = 'logloss'
     RANDOMSTATE = 42
     TREEMETHOD = 'hist'
-    EARLYSTOPPINGROUNDS = 100
+    EARLYSTOPPINGROUNDS = 200
+    VERBOSE=False
     model = XGBClassifier(
             n_estimators = NESTIMATOR,
             max_depth = MAXDEPTH,
@@ -52,10 +53,14 @@ for i in range(3, len(allSeasons)):
             eval_metric = EVALMETRICS,
             random_state = RANDOMSTATE,
             tree_method = TREEMETHOD,
-            #early_stopping_rounds=EARLYSTOPPINGROUNDS
+            early_stopping_rounds=EARLYSTOPPINGROUNDS,
         )
-    model.fit(X_train, y_train, eval_set=[(X_test,y_test)])
+    model.fit(X_train, y_train, 
+              eval_set=[(X_test,y_test)],
+              verbose=VERBOSE
+              )
     preds = model.predict_proba(X_test)[:, 1]
+    preds = np.clip(preds, 0.03, 0.97)
     LL = log_loss(y_test, preds)
 
     logloss_res[testSeason] = LL
