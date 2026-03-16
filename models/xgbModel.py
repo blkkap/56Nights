22a60fe9c967b8665785b22ae2d8f666b3548bce
blkkap <dovala69@gmail.com>
2026-03-16 13:47:41 -0700

    xgb

--- models/xgbModel.py
+++ models/xgbModel.py
@@ -9,7 +9,7 @@ import numpy as np
 
 df = pd.read_csv('../data/preprocess/merged_team_matchups.csv')
 
-features = ['Tempo_Lower','Tempo_Higher','TempoDiff','TempoGap','SeedProduct','OffvsDefLow','OffvsDefHi','eFGMatchupLow','eFGMatchupHi','MarginSQ','avgOffRtg_LowerTeamID','avgOffRtg_HigherTeamID','avgDefRtg_LowerTeamID','avgDefRtg_HigherTeamID','avgNetRtg_LowerTeamID','avgNetRtg_HigherTeamID','interaction','ESQUARE','Seed_LowerTeamID','Seed_HigherTeamID','SeedGap','EloGap','NetRtgDiff','TOVDiff','RebDiff','eFGDiff','SeedDiff','WinDiff','MarginDiff','EloDiff']
+features = ['TempoDiff','TempoGap','SeedProduct','OffvsDefLow','OffvsDefHi','eFGMatchupLow','eFGMatchupHi','MarginSQ','avgOffRtg_LowerTeamID','avgOffRtg_HigherTeamID','avgDefRtg_LowerTeamID','avgDefRtg_HigherTeamID','avgNetRtg_LowerTeamID','avgNetRtg_HigherTeamID','interaction','ESQUARE','SeedGap','EloGap','NetRtgDiff','TOVDiff','RebDiff','eFGDiff','SeedDiff','WinDiff','MarginDiff','EloDiff']
 target = 'Target'
 
 df = df.dropna()
@@ -30,41 +30,46 @@ for i in range(3, len(allSeasons)):
     y_test = test_df[target]
 
 
-    NESTIMATOR= 4000
+    NESTIMATOR= 1500
     MAXDEPTH = 4
-    #MINCHILDWEIGHT = 4 
-    LR = 0.03
+    MINCHILDWEIGHT = 5 
+    LR = 0.02
     OBJ = 'binary:logistic'
     SUBSAMPLE = 0.8
-    COLSAMPLEBYTREE = 0.8 
+    COLSAMPLEBYTREE = 0.7
     EVALMETRICS = 'logloss'
     RANDOMSTATE = 42
     TREEMETHOD = 'hist'
     EARLYSTOPPINGROUNDS = 200
     VERBOSE=False
+    GAMMA = 0.2
+    REG_LAMBDA = 2
     model = XGBClassifier(
             n_estimators = NESTIMATOR,
             max_depth = MAXDEPTH,
-            #min_child_weight = MINCHILDWEIGHT,
+            min_child_weight = MINCHILDWEIGHT,
             learning_rate = LR,
-            objective = OBJ,
+            #objective = OBJ,
             subsample = SUBSAMPLE,
             colsample_bytree = COLSAMPLEBYTREE,
             eval_metric = EVALMETRICS,
             random_state = RANDOMSTATE,
             tree_method = TREEMETHOD,
-            early_stopping_rounds=EARLYSTOPPINGROUNDS,
+            #early_stopping_rounds=EARLYSTOPPINGROUNDS,
+            gamma = GAMMA,
+            reg_lambda = REG_LAMBDA
         )
     model.fit(X_train, y_train, 
               eval_set=[(X_test,y_test)],
               verbose=VERBOSE
               )
     preds = model.predict_proba(X_test)[:, 1]
-    preds = np.clip(preds, 0.03, 0.97)
+    preds = np.clip(preds, 0.025, 0.975)
     LL = log_loss(y_test, preds)
 
     logloss_res[testSeason] = LL
 
+print('Features count:', len(features))
 print('\nLog Loss per season:', logloss_res)
 print('\nAvg Log Loss per season:', sum(logloss_res.values())/ len(logloss_res))
 
